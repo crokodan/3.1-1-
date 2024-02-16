4f8d35dacf169d5cb37a634d08702c2479fb6969
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 18:22:10 +0400

    Add files via upload

---
+++ 3.1 Задание 1)/3.1 Задание 1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1_Задание_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int m = 50;
+            for (; m != 0; m /= 10)
+            {
+                int r = m % 10;
+                if (r > x) 
+                { 
+                    x = r; 
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
